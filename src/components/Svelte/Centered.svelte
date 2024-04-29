050b928f7a2fddc9d8e02dde8b42bbb56baa9378
Cancey-Dejean <you@example.com>
2024-04-28 20:54:14 -0500

    first commit

---
+++ src/components/Svelte/Centered.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let description: string;
+</script>
+
+<div class="bg-white px-6 py-24 sm:py-32 lg:px-8">
+  <div class="mx-auto max-w-2xl text-center">
+    <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">{title || "Support Center"}</h2>
+    <p class="mt-6 text-lg leading-8 text-gray-600">
+      {description || "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."}
+    </p>
+  </div>
+</div>
---
+++ src/components/Vue/Centered.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{ title?: string; description?: string }>();
+</script>
+
+<template>
+  <div class="bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="mx-auto max-w-2xl text-center">
+      <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+        {{ title || "Support center" }}
+      </h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">
+        {{
+          description ||
+          "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua."
+        }}
+      </p>
+    </div>
+  </div>
+</template>
